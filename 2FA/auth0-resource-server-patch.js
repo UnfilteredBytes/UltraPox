1358f7616ae9ead7b4e87e2f60189d051715e5f7
Pxl10 <154343913+P1xel10@users.noreply.github.com>
2024-08-25 20:28:01 -0400

    Added auth0 resource server patch.

--- 2FA/auth0-resource-server-patch.js
+++ 2FA/auth0-resource-server-patch.js
@@ -1,4 +1,5 @@
-{
+const axios = require('axios');
+let data = JSON.stringify({
   "name": "string",
   "scopes": [
     {
@@ -30,4 +31,23 @@
     "mechanism": "mtls",
     "required": true
   }
-}
+});
+
+let config = {
+  method: 'patch',
+  maxBodyLength: Infinity,
+  url: 'https://login.auth0.com/api/v2/resource-servers/:id',
+  headers: { 
+    'Content-Type': 'application/json', 
+    'Accept': 'application/json'
+  },
+  data : data
+};
+
+axios.request(config)
+.then((response) => {
+  console.log(JSON.stringify(response.data));
+})
+.catch((error) => {
+  console.log(error);
+});
